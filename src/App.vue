d1e310cb5d1d39f244ffa2e629ebb7a206123105
JonathanECH <jonathanecampos3@gmail.com>
2026-09-03 09:37:10 -0400

    Fix de Jose, en la parte de octencion en los nombre de las criptos.

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,11 @@ onMounted(() => {
   const url = 'https://data-api.coindesk.com/asset/v1/top/list?page=1&page_size=20&sort_by=CIRCULATING_MKT_CAP_USD&sort_direction=DESC&groups=ID,BASIC,SUPPLY,PRICE,MKT_CAP,VOLUME,CHANGE,TOPLIST_RANK&toplist_quote_asset=USD&api_key=4e559f31bd2377fe58a94eeb2b057f9478c4cdb86478c824746edcefbcf34007'
   fetch(url)
     .then(res => res.json())
-    .then(({ Data }) => criptomonedas.value = Data)
+    .then(({ Data }) => {
+      Data.LIST.forEach(cripto => {
+        criptomonedas.value.push(cripto)
+      })
+    })
 })
 </script>
 
@@ -35,7 +39,7 @@ onMounted(() => {
           <label for="cripto">Criptomoneda:</label>
           <select id="cripto">
             <option value="" selected disabled>-- Seleccionar --</option>
-            <option v-for="cripto in criptomonedas" :value="cripto.SYMBOL">{{ cripto }}</option>
+            <option v-for="cripto in criptomonedas" :value="cripto.SYMBOL">{{ cripto.NAME }}</option>
           </select>
         </div>
       </form>
